5dd099640409d322df2cc9e49a75f78911af888c
manu7irl <manu7irl@gmail.com>
2021-09-29 02:31:11 +0300

    Update 2_script_manual.sh
    adapted to work on raspberry pi zero W - first test

--- scripts/2_script_manual.sh
+++ scripts/2_script_manual.sh
@@ -1,52 +1,17 @@
 #!/bin/sh
 
 echo " "
-echo "   ################################################"
-echo "   ## Did you execute 1_format_extroot.sh first? ##"
-echo "   ################################################"
-echo " "
-read -p "Press [ENTER] if YES ...or [ctrl+c] to exit"
-
-
-echo " "
-echo "This script will download and install all packages form the internet"
-echo " "
-echo "   #####################################"
-echo "   ## Make sure extroot is enabled... ##"
-echo "   #####################################"
-echo " "
-read -p "Press [ENTER] to check if extroot is enabled ...or [ctrl+c] to exit"
-
-df -h;
-
-
-
-echo " "
-echo "   ############################################"
-echo "   ## Is /dev/mmcblk0p1 mounted on /overlay? ##"
-echo "   ############################################"
-echo " "
-read -p "Press [ENTER] if YES... or [ctrl+c] to exit"
-
-echo " "
-echo "   ########################################################"
-echo "   ## Make sure you've got a stable Internet connection! ##"
-echo "   ########################################################"
-echo " "
-read -p "Press [ENTER] to Continue ...or [ctrl+c] to exit"
-
-echo " "
 echo "#################"
 echo "###   SWAP    ###"
 echo "#################"
 echo " "
 
-echo "Creating swap file"
-dd if=/dev/zero of=/overlay/swap.page bs=1M count=512;
+echo "Creating a 1GB swap file"
+dd if=/dev/zero of=/swap.page bs=1M count=1024;
 echo "Enabling swap file"
 mkswap /overlay/swap.page;
 swapon /overlay/swap.page;
-mount -o remount,size=256M /tmp;
+mount -o remount,size=512M /tmp;
 
 echo "Updating rc.local for swap"
 rm /etc/rc.local;
@@ -58,7 +23,7 @@ cat << "EOF" > /etc/rc.local
 swapon /overlay/swap.page  
 
 ###expand /tmp space  
-mount -o remount,size=256M /tmp
+mount -o remount,size=512M /tmp
 
 exit 0
 EOF
